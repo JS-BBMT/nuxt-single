84d53e5a3e4e3cbdc4b00dbaed57a0fad4922fa0
JS-BBMT <reese.telecom.0419@outlook.com>
2024-05-29 05:44:49 -0700

    set the localhost config to run on local env

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,12 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
 
+  // Set the server config
+  server: {
+    host:'127.0.0.1', // default: localhost
+    port:3000 // default: 3000
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - artist-republik',
